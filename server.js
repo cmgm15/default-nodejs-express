5c2e829da4feffefacf425fc232a43725dfa91ae
Carlos Mario Gómez Mejía <cmgomezm15@gmail.com>
2020-04-11 11:02:41 -0500

    Added default port if PORT environment variable does not exist

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => res.send("Default backend image"));
 app.get("/health_check", (req, res) => res.send("Healthy"));
